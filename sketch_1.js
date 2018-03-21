f63faf4f7d6d848ebbc1d09dcb378074d841c3d0
fataris <hana.1995@mac.com>
2018-03-22 00:43:43 +0900

    Qux was born

---
+++ sketch_1.js
@@ -0,0 +1,10 @@
+function setup() {
+    background(0);
+    createCanvas(windowWidth, windowHeight); 
+    textSize(64);
+}
+
+function draw() {
+    fill(0);
+    text('Qux', width*0.5, height*0.5);
+}
